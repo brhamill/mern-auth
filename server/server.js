a2f441dffcaeddadbffb47efa48f470ae171bfb6
Brad Hamill <brad@hamill.com>
2020-01-11 09:11:40 -0700

    completed section 3

--- server/server.js
+++ server/server.js
@@ -1,14 +1,37 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+// connect to db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log('DB CONNECTION ERROR: ', err));
 
 // import routes
 const authRoutes = require('./routes/auth');
 
-// middleware
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: 'http://localhost:3000' }));
+}
+
+// middlewares
 app.use('/api', authRoutes);
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);
