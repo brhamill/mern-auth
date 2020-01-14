b44a9f71bde98bade5489a72b4e351832fcfa6d9
Brad Hamill <brad@hamill.com>
2020-01-13 22:52:09 -0700

    section 6 code

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import Signup from './auth/Signup';
+import Signin from './auth/Signin';
+import Activate from './auth/Activate';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={App} />
+        <Route path='/' exact component={App} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/signin' component={Signin} />
+        <Route path='/auth/activate/:token' component={Activate} />
       </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Routes;
